1e48bbd3fd576fcd66f11cfc0fd24752d2861049
NapXDD <19520631@gm.uit.edu.vn>
2023-04-04 19:28:37 +0700

    signup notification

--- src/components/SignupSession/SignUp.jsx
+++ src/components/SignupSession/SignUp.jsx
@@ -100,7 +100,16 @@ export default function SignUp() {
           }
         })
         .catch((err) => {
-          console.error(err)
+          toast.error(`${err.response.data}`, {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
         })
     }
   };
